7005c9338e8da311ebedc0d43bf1db0b794219a7
snowdrop <ehome_sprite@hotmail.com>
2018-05-19 12:55:54 +0800

    async test

--- src/views/page1/main.js
+++ src/views/page1/main.js
@@ -14,3 +14,22 @@ body.appendChild(div);
 setTimeout(() => {
   domUtils.addDiv('Add Me In');
 }, 1000);
+
+const f1 = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('f1');
+      resolve();
+    }, 10);
+  })
+};
+const f2 = async () => {
+  console.log('f2');
+};
+
+const f = async () => {
+  f1();
+  console.log('f');
+};
+
+f();
